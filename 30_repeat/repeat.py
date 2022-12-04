da2cffa0bb4eda737e999fe0928c6ef152096cd2
root <root@Jordys.localdomain>
2022-12-04 14:40:14 -0500

    first commit of the exercises

---
+++ 30_repeat/repeat.py
@@ -0,0 +1,30 @@
+def repeat(phrase, num):
+    """Return phrase, repeated num times.
+
+        >>> repeat('*', 3)
+        '***'
+
+        >>> repeat('abc', 2)
+        'abcabc'
+
+        >>> repeat('abc', 0)
+        ''
+
+    Ignore illegal values of num and return None:
+
+        >>> repeat('abc', -1) is None
+        True
+
+        >>> repeat('abc', 'nope') is None
+        True
+    """
+    result = ''
+    n = 0
+    while (n < num): 
+        result += phrase
+        n += 1 
+
+    if num < 0: 
+        return None
+
+    return result 
